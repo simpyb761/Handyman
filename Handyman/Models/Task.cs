ac7482b3a5eb91fe4727373e10233ed2d5a57f83
simpyb761 <simpyb761@gmail.com>
2023-11-21 17:35:20 -0500

    Initial Create

---
+++ Handyman/Models/Task.cs
@@ -0,0 +1,29 @@
+﻿namespace Handyman.Models
+{
+    public enum Categories
+    {
+        Auto,
+        Home,
+        Lawn
+    }
+    public enum SkillLevel
+    {
+        Beginner,
+        Intermediate,
+        Expert
+    }
+    public class Tasks
+    {
+        public int DIYID { get; set; }
+        public string TaskName { get; set; }
+        public string Tools { get; set; }
+        public string Steps { get; set; }
+        public Categories Category { get; set; }
+        public SkillLevel SkillLevel { get; set; }
+        public int Duration { get; set; }
+        public string VideoLink { get; set; }
+        public int EstimatedPrice { get; set; }
+
+
+    }
+}
