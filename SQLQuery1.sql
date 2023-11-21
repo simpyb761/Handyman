cc492879fe61e4d8460e2531fac1813013d1b8ac
mel348 <emanm@mail.nmc.edu>
2023-11-21 15:25:17 -0500

    Initial Push

---
+++ SQLQuery1.sql
@@ -0,0 +1,7 @@
+﻿BULK INSERT dbo.HandmanTable
+FROM 'C:\Users\melis\OneDrive\Desktop\CIT 255\Handyman\HandymanCSV.xlsx'
+WITH (
+   FIELDTERMINATOR = ',',
+   ROWTERMINATOR = '\n',
+   FIRSTROW = 2 -- If the first row is headers, skip it
+)
